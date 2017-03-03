56d0edae079195dcfcb2dbc8b3af89685136298a
e1himself <ivan.voskoboinyk@gmail.com>
2017-03-03 16:37:27 +0200

    No magic getters and setters

--- src/ServiceContainer.php
+++ src/ServiceContainer.php
@@ -818,27 +818,4 @@ class Container
         $this->bindings = [];
         $this->instances = [];
     }
-    
-    /**
-     * Dynamically access container services.
-     *
-     * @param  string  $key
-     * @return mixed
-     */
-    public function __get($key)
-    {
-        return $this[$key];
-    }
-    
-    /**
-     * Dynamically set container services.
-     *
-     * @param  string  $key
-     * @param  mixed   $value
-     * @return void
-     */
-    public function __set($key, $value)
-    {
-        $this[$key] = $value;
-    }
 }
