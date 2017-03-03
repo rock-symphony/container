fb55b23b7fab90b684fe5d73541c32407db0ea31
e1himself <ivan.voskoboinyk@gmail.com>
2017-03-03 16:31:17 +0200

    Drop ArrayAccess interface implementation

--- src/ServiceContainer.php
+++ src/ServiceContainer.php
@@ -2,7 +2,6 @@
 namespace RockSymfony\ServiceContainer;
 
 use Closure;
-use ArrayAccess;
 use LogicException;
 use ReflectionClass;
 use ReflectionMethod;
@@ -11,7 +10,7 @@ use ReflectionParameter;
 use InvalidArgumentException;
 use RockSymfony\ServiceContainer\Exceptions\BindingResolutionException;
 
-class Container implements ArrayAccess
+class Container
 {
     /**
      * An array of the types that have been resolved.
@@ -867,62 +866,6 @@ class Container implements ArrayAccess
         $this->resolved = [];
         $this->bindings = [];
         $this->instances = [];
-    }
-    
-    /**
-     * Determine if a given offset exists.
-     *
-     * @param  string  $key
-     * @return bool
-     */
-    public function offsetExists($key)
-    {
-        return $this->bound($key);
-    }
-    
-    /**
-     * Get the value at a given offset.
-     *
-     * @param  string  $key
-     * @return mixed
-     */
-    public function offsetGet($key)
-    {
-        return $this->make($key);
-    }
-    
-    /**
-     * Set the value at a given offset.
-     *
-     * @param  string  $key
-     * @param  mixed   $value
-     * @return void
-     */
-    public function offsetSet($key, $value)
-    {
-        // If the value is not a Closure, we will make it one. This simply gives
-        // more "drop-in" replacement functionality for the Pimple which this
-        // container's simplest functions are base modeled and built after.
-        if (! $value instanceof Closure) {
-            $value = function () use ($value) {
-                return $value;
-            };
-        }
-        
-        $this->bind($key, $value);
-    }
-    
-    /**
-     * Unset the value at a given offset.
-     *
-     * @param  string  $key
-     * @return void
-     */
-    public function offsetUnset($key)
-    {
-        $key = $this->normalize($key);
-        
-        unset($this->bindings[$key], $this->instances[$key], $this->resolved[$key]);
     }
     
     /**
