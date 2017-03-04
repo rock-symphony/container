c67c7c9206c6b9139bb6ad5fb26dd2869ba470e1
e1himself <ivan.voskoboinyk@gmail.com>
2017-03-04 17:40:37 +0200

    Add test case for basic `set` and `get`

---
+++ tests/SetAndGetTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace RockSymfony\ServiceContainer\Tests;
+
+use PHPUnit\Framework\TestCase;
+use RockSymfony\ServiceContainer\ServiceContainer;
+
+class SetAndGetTest extends TestCase
+{
+    /** @var ServiceContainer */
+    private $container;
+    
+    protected function setUp()
+    {
+        $this->container = new ServiceContainer();
+    }
+    
+    /**
+     * @test
+     */
+    public function it_should_set_services()
+    {
+        $this->assertFalse($this->container->isBound('test'));
+        $this->assertFalse($this->container->isResolved('test'));
+        
+        $this->container->instance('test', $this);
+    
+        $this->assertTrue($this->container->isBound('test'));
+        $this->assertTrue($this->container->isResolved('test'));
+    }
+    
+    /**
+     * @test
+     * @depends it_should_set_services
+     */
+    public function it_should_get_services()
+    {
+        $this->container->instance('test', $this);
+        
+        $test = $this->container->resolve('test');
+    
+        $this->assertInstanceOf(__CLASS__, $test);
+        $this->assertSame($this, $test);
+    }
+}
