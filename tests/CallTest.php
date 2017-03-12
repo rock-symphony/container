7407e7aa8821cb5fe1381b24de3dddb049748da9
e1himself <ivan.voskoboinyk@gmail.com>
2017-03-12 12:45:31 +0200

    Don't use PHP 5.5 features just yet

--- tests/CallTest.php
+++ tests/CallTest.php
@@ -2,7 +2,6 @@
 namespace RockSymphony\ServiceContainer\Tests;
 
 use PHPUnit\Framework\TestCase;
-use RockSymphony\ServiceContainer\Exceptions\BindingResolutionException;
 use RockSymphony\ServiceContainer\ServiceContainer;
 
 /**
@@ -10,6 +9,8 @@ use RockSymphony\ServiceContainer\ServiceContainer;
  */
 class CallTest extends TestCase
 {
+    const CLASS_NAME = __CLASS__;
+    
     /** @var ServiceContainer */
     private $container;
     
@@ -34,7 +35,7 @@ class CallTest extends TestCase
     
     public static function inject_and_perform_assertions_statically(CallTest $test, $challenge)
     {
-        $test->assertInstanceOf(CallTest::class, $test);
+        $test->assertInstanceOf(CallTest::CLASS_NAME, $test);
         
         return $challenge + 1; // to make sure the method is called
     }
@@ -44,7 +45,7 @@ class CallTest extends TestCase
      */
     public function it_should_call_instance_methods()
     {
-        $this->container->set(CallTest::class, $this);
+        $this->container->set(CallTest::CLASS_NAME, $this);
         
         $result = $this->container->call([$this, 'inject_and_perform_assertions'], ['challenge' => 1]);
         
@@ -56,13 +57,13 @@ class CallTest extends TestCase
      */
     public function it_should_call_static_methods()
     {
-        $this->container->set(CallTest::class, $this);
+        $this->container->set(CallTest::CLASS_NAME, $this);
         
-        $result = $this->container->call([CallTest::class, 'inject_and_perform_assertions_statically'], ['challenge' => 2]);
+        $result = $this->container->call([CallTest::CLASS_NAME, 'inject_and_perform_assertions_statically'], ['challenge' => 2]);
         
         $this->assertEquals(3, $result);
     
-        $result = $this->container->call(CallTest::class . '::inject_and_perform_assertions_statically', ['challenge' => 3]);
+        $result = $this->container->call(CallTest::CLASS_NAME . '::inject_and_perform_assertions_statically', ['challenge' => 3]);
     
         $this->assertEquals(4, $result);
     }
@@ -72,9 +73,9 @@ class CallTest extends TestCase
      */
     public function it_should_throw_exception_if_required_parameter_with_no_hint_is_not_specified()
     {
-        $this->container->set(CallTest::class, $this);
+        $this->container->set(CallTest::CLASS_NAME, $this);
         
-        $this->expectException(BindingResolutionException::class);
+        $this->expectException('RockSymphony\ServiceContainer\Exceptions\BindingResolutionException');
         
         $this->container->call([$this, 'inject_and_perform_assertions']);
     }
@@ -84,7 +85,7 @@ class CallTest extends TestCase
      */
     public function it_should_automatically_create_missing_class_dependencies()
     {
-        $result = $this->container->call(CallTest::class . '@construct_and_perform_assertions', ['challenge' => 5]);
+        $result = $this->container->call(CallTest::CLASS_NAME . '@construct_and_perform_assertions', ['challenge' => 5]);
         
         $this->assertEquals(6, $result);
     }
@@ -94,9 +95,9 @@ class CallTest extends TestCase
      */
     public function it_should_call_bound_services_methods_with_at_sign_notation()
     {
-        $this->container->set(CallTest::class, $this);
+        $this->container->set(CallTest::CLASS_NAME, $this);
         
-        $result = $this->container->call(CallTest::class . '@inject_and_perform_assertions', ['challenge' => 7]);
+        $result = $this->container->call(CallTest::CLASS_NAME . '@inject_and_perform_assertions', ['challenge' => 7]);
         
         $this->assertEquals(8, $result);
     }
@@ -106,7 +107,7 @@ class CallTest extends TestCase
      */
     public function it_should_construct_unbound_instances_and_call_methods_with_at_sign_notation()
     {
-        $result = $this->container->call(CallTest::class . '@construct_and_perform_assertions', ['challenge' => 7]);
+        $result = $this->container->call(CallTest::CLASS_NAME . '@construct_and_perform_assertions', ['challenge' => 7]);
         
         $this->assertEquals(8, $result);
     }
@@ -116,7 +117,7 @@ class CallTest extends TestCase
      */
     public function it_should_inject_dependencies_into_closures()
     {
-        $this->container->set(CallTest::class, $this);
+        $this->container->set(CallTest::CLASS_NAME, $this);
         
         $result = $this->container->call(function (CallTest $test, $challenge) {
             $this->assertSame($this, $test);
